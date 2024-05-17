e49a88f66be8406b58666ef7fc13d6ef0d3a6cb4
Dylan O’Brien <dylanobrien@Dylans-Air-2.wit.ie>
2024-05-17 16:28:47 +0100

    Added Movies Route

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,6 +4,8 @@ import cors from 'cors';
 import usersRouter from './api/users';
 import './db';
 import defaultErrHandler from './errHandler'
+import moviesRouter from './api/movies';   //import movies router
+
 
 dotenv.config();
 
@@ -11,9 +13,15 @@ const app = express();
 const port = process.env.PORT; 
 
 app.use(cors());
+
 app.use(express.json());
+
 app.use('/api/users', usersRouter);
+
+app.use('/api/movies', moviesRouter); 
+
 app.use(defaultErrHandler);
+
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
