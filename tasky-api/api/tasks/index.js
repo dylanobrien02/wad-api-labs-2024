93439672906bfd96b33a7cc75970b0143455bd5f
Dylan O’Brien <dylanobrien@Dylans-Air-2.wit.ie>
2024-05-17 12:29:41 +0100

    Mongo lab Error Handling

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import asyncHandler from 'express-async-handler';
 import Task from './taskModel';
 
-const router = express.Router(); // eslint-disable-line
+const router = express.Router();
 
 // Get all tasks
 router.get('/', async (req, res) => {
@@ -9,11 +10,12 @@ router.get('/', async (req, res) => {
     res.status(200).json(tasks);
 });
 
-// create a task
-router.post('/', async (req, res) => {
+// Create a task
+router.post('/', asyncHandler(async (req, res) => {
     const task = await Task(req.body).save();
     res.status(201).json(task);
-});
+}));
+
 // Update Task
 router.put('/:id', async (req, res) => {
     if (req.body._id) delete req.body._id;
@@ -21,13 +23,13 @@ router.put('/:id', async (req, res) => {
         _id: req.params.id,
     }, req.body);
     if (result.matchedCount) {
-        res.status(200).json({ code:200, msg: 'Task Updated Sucessfully' });
+        res.status(200).json({ code:200, msg: 'Task Updated Successfully' });
     } else {
         res.status(404).json({ code: 404, msg: 'Unable to find Task' });
     }
 });
 
-// delete Task
+// Delete Task
 router.delete('/:id', async (req, res) => {
     if (req.body._id) delete req.body._id;
     const result = await Task.deleteOne({
